b81c13b5ddd38eae79aae8c9e410874699613b00
User <nikita96max@gmail.com>
2022-02-05 23:32:20 +0500

    Create project and add first MainActivity and additional Fragment

---
+++ app/src/main/java/we/itschool/project/traveler/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package we.itschool.project.traveler.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import we.itschool.project.traveler.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
