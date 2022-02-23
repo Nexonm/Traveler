de5db8853545dda7dad764a78d705d2d531f3c9c
Alex Mak <arfa3.3@mail.ru>
2022-02-23 19:52:41 +0500

    create Person && PersonInfo

---
+++ app/src/main/java/we/itschool/project/traveler/domain/entity/PersonInfo.java
@@ -0,0 +1,49 @@
+package we.itschool.project.traveler.domain.entity;
+
+public class PersonInfo {
+    private final String firstName;
+    private final String secondName;
+    private final String email;
+    private final String socialContacts;
+    private final String pathToPhoto;
+    private final String dateOfBirth;
+
+    public PersonInfo(
+            String firstName,
+            String secondName,
+            String email,
+            String socialContacts,
+            String pathToPhoto,
+            String dateOfBirth) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+        this.socialContacts = socialContacts;
+        this.dateOfBirth = dateOfBirth;
+        this.pathToPhoto = pathToPhoto;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSocialContacts() {
+        return socialContacts;
+    }
+
+    public String getPathToPhoto() {
+        return pathToPhoto;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
