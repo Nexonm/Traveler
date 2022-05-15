29cb49e93b5d72469f64bfc6d3b992b486f24518
User <nikita96max@gmail.com>
2022-05-15 14:42:42 +0500

    make real url to server

--- app/src/main/java/we/itschool/project/traveler/data/api/APIConfig.java
+++ app/src/main/java/we/itschool/project/traveler/data/api/APIConfig.java
@@ -1,6 +1,6 @@
 package we.itschool.project.traveler.data.api;
 
 public class APIConfig {
-    public static final String HOST_URL = "http://fb06-188-17-189-43.ngrok.io/";
+    public static final String HOST_URL = "http://51.250.110.228:8080/";
     public static final String STORAGE_CARD_PHOTO_METHOD = HOST_URL+"storage/image-card?cid="; //needs card id
 }
