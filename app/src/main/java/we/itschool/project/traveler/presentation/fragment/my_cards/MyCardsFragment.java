caf3c986728a744e280f21cc0111b9c18795f6eb
User <nikita96max@gmail.com>
2022-05-15 18:21:23 +0500

    add my cards working feed

--- app/src/main/java/we/itschool/project/traveler/presentation/fragment/my_cards/MyCardsFragment.java
+++ app/src/main/java/we/itschool/project/traveler/presentation/fragment/my_cards/MyCardsFragment.java
@@ -10,11 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
 
 import we.itschool.project.traveler.R;
 import we.itschool.project.traveler.databinding.FragmentMyCardsBinding;
+import we.itschool.project.traveler.domain.entity.CardEntity;
+import we.itschool.project.traveler.presentation.fragment.card_big.CardFragment;
+import we.itschool.project.traveler.presentation.fragment.card_list.Adapter;
+import we.itschool.project.traveler.presentation.fragment.card_list.DiffCallback;
 import we.itschool.project.traveler.presentation.fragment.create_new_card.CreateNewCardFragment;
 
 public class MyCardsFragment extends Fragment {
@@ -22,6 +29,10 @@ public class MyCardsFragment extends Fragment {
     private FragmentMyCardsBinding binding;
 
     private FloatingActionButton fb_new_card;
+
+    private CardListViewModelMyCards viewModel;
+    private RecyclerView recyclerView;
+    private Adapter adapter;
 
     public static MyCardsFragment newInstance(){
         return new MyCardsFragment();
@@ -45,7 +56,22 @@ public class MyCardsFragment extends Fragment {
             @NonNull View view,
             @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initAdapter();
+        initViewModel();
         initView(view);
+    }
+
+    private void initAdapter() {
+        adapter = new Adapter(new DiffCallback());
+        adapter.cardClickListener = this::startCardFragment;
+    }
+
+    private void initViewModel() {
+        viewModel = new ViewModelProvider(this).get(CardListViewModelMyCards.class);
+        viewModel.getCardList().observe(
+                getViewLifecycleOwner(),
+                cards -> adapter.submitList(cards)
+        );
     }
 
     private void initView(View view) {
@@ -53,6 +79,11 @@ public class MyCardsFragment extends Fragment {
         fb_new_card.setOnClickListener(v -> {
             startCreateNewCardFragment();
         });
+
+        recyclerView = view.findViewById(R.id.rv_card_list_my_cards);
+        recyclerView.setAdapter(adapter);
+        recyclerView.getRecycledViewPool().setMaxRecycledViews(
+                Adapter.VIEW_TYPE_CARD_VISITOR, Adapter.MAX_POOL_SIZE);
     }
 
     private void startCreateNewCardFragment(){
@@ -71,5 +102,19 @@ public class MyCardsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void startCardFragment(CardEntity card) {
+
+        Fragment fragment = CardFragment.newInstance(
+                (new Gson()).toJson(card)
+        );
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.popBackStack();
+        fragmentManager
+                .beginTransaction()
+                .addToBackStack("null")
+                .replace(R.id.nav_host_fragment_content_main, fragment, null)
+                .commit();
     }
 }
