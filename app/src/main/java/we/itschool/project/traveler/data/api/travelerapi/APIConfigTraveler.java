fe284d677874a033d2dcad5f58cf7b9c48447cc3
User <nikita96max@gmail.com>
2022-05-22 20:06:41 +0500

    add new method string

--- app/src/main/java/we/itschool/project/traveler/data/api/travelerapi/APIConfigTraveler.java
+++ app/src/main/java/we/itschool/project/traveler/data/api/travelerapi/APIConfigTraveler.java
@@ -5,4 +5,5 @@ public class APIConfigTraveler {
     public static final String HOST_URL = "http://51.250.110.228:8080/";
     //path to get card
     public static final String STORAGE_CARD_PHOTO_METHOD = HOST_URL+"storage/image-card?cid="; //needs card id
+    public static final String STORAGE_USER_PHOTO_METHOD = HOST_URL+"storage/image-user?uid="; //needs card id
 }
