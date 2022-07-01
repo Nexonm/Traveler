be107bb891951ebc28058cc61db005ad09a597e8
User <nikita96max@gmail.com>
2022-07-02 01:06:02 +0500

    add card answers

---
+++ data/src/main/java/traveler/module/data/travelerapi/errors/CardNetAnswers.java
@@ -0,0 +1,7 @@
+package traveler.module.data.travelerapi.errors;
+
+public class CardNetAnswers {
+
+    //errors from server
+    public final static String cardWasDeleted = "Карта с таким id была удалена";
+}
