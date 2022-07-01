6fdd9e6ed78cd3faaf99171160d60d56501a2e22
User <nikita96max@gmail.com>
2022-07-01 22:43:44 +0500

    delete comments (cleancode)

--- data/src/main/java/traveler/module/data/usecasesimplementation/user/UserLoginUCImpl.java
+++ data/src/main/java/traveler/module/data/usecasesimplementation/user/UserLoginUCImpl.java
@@ -1,7 +1,5 @@
 package traveler.module.data.usecasesimplementation.user;
 
-import android.util.Log;
-
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -25,37 +23,28 @@ public class UserLoginUCImpl implements UserLoginUCI {
         try {
             String ans = "waiting";
             Response<String> response = call.execute();
-            Log.v("OkHttpClient LOGIN", "Response.isSuccessful() = " + response.isSuccessful());
+
             if (response.isSuccessful()) {
-                Log.v("OkHttpClient LOGIN", "response.body() = " + response.body());
                 ans = response.body().toString();
             } else {
-//                Log.v("OkHttpClient LOGIN", "response.errorBody().string = " + response.errorBody().string());
                 ans = response.errorBody().string().toString();
-                Log.v("OkHttpClient LOGIN", "(ans) response.errorBody().string = " + ans);
-
             }
-            Log.v("OkHttpClient LOGIN", "Check what is ANS: " + ans);
 
             if (UserNetAnswers.userDoesNotExistException.equals(ans) ||
                     UserNetAnswers.userIncorrectPasswordException.equals(ans)) {
-                Log.v("OkHttpClient LOGIN", "Exception from server!!!");
                 return new AnswerUserAndString(null, ans);
             } else {
                 try {
-                    Log.v("OkHttpClient LOGIN", "User is logged, user creation started");
                     return new AnswerUserAndString(
                             UserEntityMapper.toUserEntityFormUserServ(
                                     (new Gson()).fromJson(ans, UserServ.class),
                                     true),
                             UserNetAnswers.userSuccessLogin);
                 } catch (Exception e) {
-                    Log.v("OkHttpClient LOGIN", "Exception while try make usr from answer: " + e.getMessage().toString());
                     return new AnswerUserAndString(null, UserNetAnswers.userOtherError);
                 }
             }
         } catch (IOException e) {
-            Log.v("OkHttpClient LOGIN", "Exception in all request");
             return new AnswerUserAndString(null, UserNetAnswers.userOtherError);
         }
     }
