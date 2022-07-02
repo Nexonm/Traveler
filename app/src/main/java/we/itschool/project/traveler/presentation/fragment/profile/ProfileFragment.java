ca9e9c2918faa96eb71c6f62ff640089717b8213
User <nikita96max@gmail.com>
2022-07-03 01:45:37 +0500

    delete cursor

--- app/src/main/java/we/itschool/project/traveler/presentation/fragment/profile/ProfileFragment.java
+++ app/src/main/java/we/itschool/project/traveler/presentation/fragment/profile/ProfileFragment.java
@@ -192,7 +192,6 @@ public class ProfileFragment extends Fragment {
         Cursor cursor = this.requireActivity().getContentResolver().query(uri, null, null, null, null);
         cursor.moveToFirst();
         int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        cursor.close();
         return cursor.getString(idx);
     }
 
